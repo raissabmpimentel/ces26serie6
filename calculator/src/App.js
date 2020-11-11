ce1d2ffb926e40e6c20145394b6c1ec17e79180d
Raissa Pimentel <raissabmpimentel@gmail.com>
2020-11-11 16:40:24 -0300

    First files

---
+++ calculator/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render(){
+    return (
+      <div style={{marginLeft: "10px"}}>
+          <h1>CES-26 - 6ª Série de Exercícios</h1>
+          <h2>Aluna: Raíssa Batista de Miranda Pimentel - Turma: COMP-21</h2>
+          
+      </div>
+    );
+  }
+  
+}
+
+export default App;
